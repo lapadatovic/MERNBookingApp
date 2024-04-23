49508e2fcc7ba16cca85b410e816f76887f8e3b5
lapadatovic <lapadatovic.dev@gmail.com>
2024-04-24 00:29:56 +0200

    Fi with backend side of register and auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import userRoutes from './routes/users'
+import authRoutes from './routes/auth';
 
 // connect to DB
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string, {});
@@ -12,10 +14,12 @@ app.use(express.urlencoded({extended: true}))
 app.use(cors());
 
 
+// app.get('/api/test', async (req: Request, res: Response ) => {
+//     res.json({message: "Hello from express endpoint"});
+// });
 
-app.get('/api/test', async (req: Request, res: Response ) => {
-    res.json({message: "Hello from express endpoint"});
-});
+app.use('/api/users', userRoutes)
+app.use('/api/users', authRoutes);
 
 app.listen(3500, () => {
     console.log("Server running at PORT:3500")
---
+++ backend/src/routes/users.ts
@@ -0,0 +1,58 @@
+import express, { Request, Response } from 'express';
+import User from '../models/user'
+import jwt from 'jsonwebtoken'
+import {validationResult,check} from 'express-validator'
+const router = express.Router();
+
+
+// /api/users/register
+router.post("/register", 
+        [ 
+            check('firstName', "First Name is required").isString(),
+            check('lastName', "Last Name is required").isString(),
+            check('email', "Email is required").isEmail(),
+            check('password', "Password with 6 or more characters required").isLength({min:6}),
+        ], 
+        async (req: Request, res: Response) => {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(400).json({message: errors.array()});
+            }
+
+            try {
+                let user = await User.findOne({
+                    email: req.body.email
+                });
+
+                if(user) return res.status(400).json({ status: "error", message: "User already exist"});
+
+                // If there is not user matching, we create new user with data from req.body
+                user = new User(req.body);
+                // after that we save new user
+                await user.save();
+
+                const token = jwt.sign(
+                    {userId: user.id}, 
+                    process.env.JWT_SECRET_KEY as string,
+                    {expiresIn: '1d'}
+                );
+
+                res.cookie("auth_token", token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    // 1d in ms
+                    maxAge: 86400000,
+
+                });
+
+                return res.sendStatus(200);
+
+            } catch (error) {
+                console.log(error)
+                res.status(500).json({status: "erorr", message: "Something went wrong!"}); 
+            }
+        }
+)
+
+
+export default router;
