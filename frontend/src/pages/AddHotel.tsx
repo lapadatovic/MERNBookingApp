6046126762b5c06fbdff246974d54a8d1cb20553
lapadatovic <lapadatovic.dev@gmail.com>
2024-05-15 02:33:58 +0200

    Created input base input fields for AddHotel

---
+++ frontend/src/pages/AddHotel.tsx
@@ -0,0 +1,11 @@
+import ManageHotelForm from "../forms/ManageHotelForm";
+
+const AddHotel = () => {
+    return (
+        <div>
+            <ManageHotelForm />
+        </div>
+    )
+}
+
+export default AddHotel;
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,7 +1,10 @@
 import Register from "./Register";
 import SignIn from "./SignIn";
+import AddHotel from "./AddHotel";
 
 export {
     Register,
     SignIn,
+    AddHotel,
+    
 }
