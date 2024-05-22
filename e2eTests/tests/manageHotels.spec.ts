0e43e92ed299adeba6334e89bd745c6e43d6e244
lapadatovic <lapadatovic.dev@gmail.com>
2024-05-22 20:23:00 +0200

    MyHotels e2e tests

--- e2eTests/tests/manageHotels.spec.ts
+++ e2eTests/tests/manageHotels.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/test';
-import exp from 'constants';
 import path from 'path';
 
 const UI_URL= 'http://localhost:5173';
@@ -54,4 +53,22 @@ test('Should allow user to add a hotel',async ({page}) => {
 
     await expect(page.getByText('Hotel Saved')).toBeVisible({ timeout: 20000 });
 
+});
+
+
+test("Should display hotels" , async ({page}) => {
+    // await page.getByRole('link', {name: 'My Hotels'}).click();
+    await page.goto(`${UI_URL}/my-hotels`);
+
+    await expect(page.getByRole('heading', {name: 'My Hotels'})).toBeVisible();
+    await expect(page.getByText('Dublin Getaways')).toBeVisible();
+    await expect(page.getByText("Lorem ipsum dolor sit amet")).toBeVisible();
+    await expect(page.getByText('Dublin, Ireland')).toBeVisible();
+    await expect(page.getByText('All Inclusive')).toBeVisible();
+    await expect(page.getByText('$119 per night')).toBeVisible();
+    await expect(page.getByText('2 adults, 3 children')).toBeVisible();
+    await expect(page.getByText('5 Star Rating')).toBeVisible();
+    
+    await expect(page.getByRole('link', {name: 'View Details'})).toBeVisible();
+    await expect(page.getByRole('link', {name: 'Add Hotel'})).toBeVisible();
 });
--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -30,7 +30,8 @@ const MyHotels = ( ) => {
 
             <div className="grid grid-cols-1 gap-8">
                 {hotelData?.map((hotel) => (
-                    <div 
+                    <div
+                        key={hotel._id}
                         className="flex flex-col justify-between border border-slate-300 rounded-lg p-8 gap-5"
                     > 
                         <h2 className=" text-2xl font-bold">{hotel.name}</h2>
@@ -70,7 +71,6 @@ const MyHotels = ( ) => {
                     </div>
                 ))}
             </div>
-
         </div>
     )
 }
