e1806bf62cfd2cc3900488351ca7a21b1b895e21
lapadatovic <lapadatovic.dev@gmail.com>
2024-05-29 16:09:08 +0200

    Edit Hotel Page done

--- frontend/src/api-clients.ts
+++ frontend/src/api-clients.ts
@@ -85,3 +85,28 @@ export const fetchMyHotels = async (): Promise<HotelType[]> => {
 
     return response.json();
 }
+
+
+export const fetchMyHotelById = async (hotelId: string): Promise<HotelType> => {
+    const response = await fetch(`${API_BASE_URL}/api/my-hotels/${hotelId}`, {
+        credentials: 'include',
+    });
+
+    if(!response.ok) throw new Error('Failed to fetch hotel with ID');
+
+    return response.json();
+}
+
+export const updateMyHotelById = async (hotelFormData: FormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/my-hotels/${hotelFormData.get('hotelId')}`, {
+        method: 'PUT',
+        body: hotelFormData,
+        credentials: 'include'
+    });
+
+    if(!response.ok) {
+        throw new Error('Failed toupdate hotel')
+    };
+
+    return response.json();
+}   
