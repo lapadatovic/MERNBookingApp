1eeb1371789c3b612c02dc548f2031cda05a3066
lapadatovic <lapadatovic.dev@gmail.com>
2024-06-19 03:20:26 +0200

    DetailsPage tested

--- e2eTests/tests/search-hotels.spec.ts
+++ e2eTests/tests/search-hotels.spec.ts
@@ -20,15 +20,13 @@ test.beforeEach(async ({ page }) => {
 
 test("Should show hotel serach results", async ({page}) => {
     await page.goto(UI_URL);
-    let startDate = '16/06/2024';
-    let endDate = '20/06/2024';
-
+  
     await page.getByPlaceholder('Where are you going?').fill('Dublin');
     await page.getByLabel('Adults:').fill('2');
     await page.getByLabel('Children').fill('1');
     // checkIn date
     await page.getByPlaceholder('Check-in Date').click();
-    await page.getByRole('option').getByText('16').click();
+    await page.getByRole('option').getByText('19').click();
     // checkout date
     await page.getByPlaceholder('Check-out Date').click();
     const targetDiv = await page.$('div[aria-label="Choose Friday, June 28th, 2024"]');
@@ -38,4 +36,30 @@ test("Should show hotel serach results", async ({page}) => {
     
     await expect(page.getByText('Hotels found in Dublin')).toBeVisible();
     await expect(page.getByRole('link', {name: 'Dublin Getaways'})).toBeVisible();
+});
+
+test("Should show hotel detail", async ({page}) => {
+    await page.goto(UI_URL);
+
+    await page.getByPlaceholder('Where are you going?').fill('Dublin');
+    await page.getByLabel('Adults:').fill('2');
+    await page.getByLabel('Children').fill('1');
+    // checkIn date
+    await page.getByPlaceholder('Check-in Date').click();
+    await page.getByRole('option').getByText('19').click();
+    // checkout date
+    await page.getByPlaceholder('Check-out Date').click();
+    const targetDiv = await page.$('div[aria-label="Choose Friday, June 28th, 2024"]');
+    await targetDiv?.click();
+
+    await page.getByRole('button', {name: 'Search'}).click();
+    
+    await expect(page.getByText('Hotels found in Dublin')).toBeVisible();
+    await expect(page.getByRole('link', {name: 'Dublin Getaways'})).toBeVisible();
+
+    await page.getByRole('link', {name: 'Dublin Getaways'}).click();
+    await expect(page.getByRole('heading', {name: 'Dublin Getaways'})).toBeVisible();
+    await expect(page).toHaveURL(/detail/);
+    await expect(page.getByRole('button', {name: 'Book now'})).toBeVisible();
+
 });
